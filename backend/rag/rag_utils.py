5ef35960096bc15dc01a928655137c9986cef56e
Edward Sun <edward.sun2015@gmail.com>
2024-04-20 15:05:39 -0700

    langchain draft done

--- backend/rag/rag_utils.py
+++ backend/rag/rag_utils.py
@@ -4,6 +4,7 @@ import sys
 from langchain import hub
 import chromadb
 from langchain_community.document_loaders import TextLoader
+from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_core.output_parsers import StrOutputParser
 from langchain_chroma import Chroma
@@ -11,6 +12,11 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.chat_history import BaseChatMessageHistory 
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 data_path = os.path.dirname("../data/")
  
@@ -21,21 +27,71 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 splits = text_splitter.split_documents(docs)
 vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-# Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever()
+# retriever with only 1 document in Chromadb
+retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 prompt = hub.pull("rlm/rag-prompt")
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+### Contextualize question ###
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
 )
 
-print(rag_chain.invoke("What is my condition"))
+### Answer question ###
+qa_system_prompt = """You are an health care assistant for question-answering tasks. \
+Use the following pieces of retrieved context regarding the patient's condition from a doctors visit transcript to answer the question posed by the patient. \
+If you don't know the answer, just say that you don't know. Only answer questions related to health. \
+Keep the answer concise.\
+
+{context}"""
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
+### Statefully manage chat history ###
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+while True:
+    user = input(">")
+
+    print(conversational_rag_chain.invoke(
+        {"input": f"{user}"},
+        config={
+            "configurable": {"session_id": "abc123"}
+        },  # constructs a key "abc123" in `store`.
+    )["answer"])
