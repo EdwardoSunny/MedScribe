35dd5ff5114e8be18fd1f238ed4e8008c509495c
Edward Sun <edward.sun2015@gmail.com>
2024-04-20 14:16:44 -0700

    ignroed data

--- backend/rag/rag_utils.py
+++ backend/rag/rag_utils.py
@@ -1,11 +1,29 @@
 import os
 import sys
-import bs4
-from langchain import hub
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_chroma import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from langchain import hub
+import chromadb
+from langchain_community.document_loaders import TextLoader
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+ 
+loader = TextLoader("../../modules/state_of_the_union.txt")
+documents = loader.load()
+
+# split it into chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+# create the open-source embedding function
+embedding_function = OpenAIEmbeddings()
+
+# load it into Chroma
+db = Chroma.from_documents(docs, embedding_function)
+
+# query it
+query = "What did the president say about Ketanji Brown Jackson"
+docs = db.similarity_search(query)
+
+# print results
+print(docs[0].page_content)
