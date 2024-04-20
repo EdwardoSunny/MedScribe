08773de9b3244fec9e3701099182dc941cfc02ee
Edward Sun <edward.sun2015@gmail.com>
2024-04-20 14:31:06 -0700

    draft of langchain done

--- backend/rag/rag_utils.py
+++ backend/rag/rag_utils.py
@@ -4,26 +4,38 @@ import sys
 from langchain import hub
 import chromadb
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+data_path = os.path.dirname("../data/")
  
-loader = TextLoader("../../modules/state_of_the_union.txt")
-documents = loader.load()
+loader = TextLoader(os.path.join(data_path, "transcript_1.txt"))
+docs = loader.load()
 
-# split it into chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-# create the open-source embedding function
-embedding_function = OpenAIEmbeddings()
+# Retrieve and generate using the relevant snippets of the blog.
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
-# load it into Chroma
-db = Chroma.from_documents(docs, embedding_function)
 
-# query it
-query = "What did the president say about Ketanji Brown Jackson"
-docs = db.similarity_search(query)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
-# print results
-print(docs[0].page_content)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+print(rag_chain.invoke("What is my condition"))
