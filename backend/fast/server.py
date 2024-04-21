34517b4bf9b35ea896893afebb32ac6fb03c0c78
Jonathan Deng <jonathandeng@Jonathans-MacBook-Air.local>
2024-04-21 02:12:40 -0700

    server.py

--- backend/fast/server.py
+++ backend/fast/server.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+import sys
+sys.path.insert(1, '../')
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from rag.rag_utils.py import ChatBot
+import requests
 
 app = FastAPI()
 
+class ChatInput(BaseModel):
+    message: str
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+
+@app.post("/chat/")
+async def generate_response(chat_input: ChatInput):
+	c = ChatBot("transcript_1.txt")
+	response = c.get_response(chat_input.message)
+        return {"message": response.choices[0].text.strip()}
