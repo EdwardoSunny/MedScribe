8db341ada867f09e75d29841e19524360914dd17
Kevin Xue <kevinxue331@gmail.com>
2024-04-20 18:37:17 -0700

    pppp

--- backend/main.py
+++ backend/main.py
@@ -2,4 +2,24 @@ from fastapi import FastAPI, File, UploadFile
 from tempfile import NamedTemporaryFile
 import os
 
-@app.post("/video/detect-faces")
+@app.post("/video/upload-video")
+def upload_video(file: UploadFile = File(...)):
+    temp = NamedTemporaryFile(delete=False)
+    try:
+        try:
+            contents = file.file.read()
+            with temp as f:
+                f.write(contents);
+        except Exception:
+            return {"message": "There was an error uploading the file"}
+        finally:
+            file.file.close()
+        
+        res = process_video(temp.name)  # Pass temp.name to VideoCapture()
+    except Exception:
+        return {"message": "There was an error processing the file"}
+    finally:
+        #temp.close()  # the `with` statement above takes care of closing the file
+        os.remove(temp.name)
+        
+    return res
