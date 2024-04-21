212b1ff884ebe113d7865a410daa791ce2167e67
Edward Sun <edward.sun2015@gmail.com>
2024-04-21 16:38:42 -0700

    final final readme

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,12 @@ bash reset.sh
 
 cd /home/edwardsun/Documents/Code/Hacks/LAHacks2024/backend/
 
+python -m venv "env"
+
 source env/bin/activate
 
-cd /home/edwardsun/Documents/Code/Hacks/LAHacks2024/frontend/ && npm run dev &
+pip install -r requirements.txt
+
+cd /home/edwardsun/Documents/Code/Hacks/LAHacks2024/frontend/ & npm install & npm run dev &
 
 cd /home/edwardsun/Documents/Code/Hacks/LAHacks2024/backend/ && python3 server.py
